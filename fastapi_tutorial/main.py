9223e79ebad20a7eb61892ebb8871e9bbe01f6de
shiv <shivpkeshri@gmail.com>
2024-08-09 20:18:38 +0530

    fastapi-get-and-post

---
+++ fastapi_tutorial/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/")
+async def post():
+    return {"message": "Hello from the post route"}
